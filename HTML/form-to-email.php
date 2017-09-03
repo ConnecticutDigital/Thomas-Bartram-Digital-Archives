197e17237bce629866080965b77b6d65c60bbcca
Rebecca Parker <rjp43@pitt.edu>
2017-09-03 13:03:44 -0500

    updated php contact form; corrected redirect issue and updated text and styling

--- HTML/form-to-email.php
+++ HTML/form-to-email.php
@@ -2,38 +2,46 @@
 if(!isset($_POST['submit']))
 {
 	//This page should not be accessed directly. Need to submit the form.
-	echo "error; you need to submit the form!";
+	echo "Error! This page should not be accessed directly. You need to submit a contact form! Please visit <a href='http://www.blackrockhistory.net/contact.html'>http://www.blackrockhistory.net/contact.html</a> to submit.";
 }
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $message = $_POST['message'];
 
+
 //Validate first
 if(empty($name)||empty($visitor_email)) 
 {
-    echo "Name and email are mandatory!";
+    echo "Error! Name and email are mandatory on contact form. Please return to <a href='http://www.blackrockhistory.net/contact.html'>http://www.blackrockhistory.net/contact.html</a> to complete your inquiry.";
+    exit;
+}
+if(empty($message))
+{
+    echo "Error! A message is mandatory. Please provide a detailed message regarding your inquiry so we can better serve you. Return to <a href='http://www.blackrockhistory.net/contact.html'>http://www.blackrockhistory.net/contact.html</a> to complete your inquiry.";
     exit;
 }
 
 if(IsInjected($visitor_email))
 {
-    echo "Bad email value!";
+    echo "Error! You have provided an invalid email value. Return to <a href='http://www.blackrockhistory.net/contact.html'>http://www.blackrockhistory.net/contact.html</a> to complete your inquiry.";
     exit;
 }
 
-$email_from = 'rjp396@gmail.com';//<== update the email address
-$email_subject = "New Form submission";
-$email_body = "You have received a new message from the user $name.\n".
-    "Here is the message:\n $message".
+$email_from = "rjp396@gmail.com";//<== update the email address
+$email_subject = "New Form Submission";
+$email_body = 
+    "You have received a new inquiry from $name.\n\n".
+    "User provided email:\n $visitor_email\n\n".
+    "Here is their detailed message:\n $message\n\n".
     
 $to = "rjp396@gmail.com";//<== update the email address
 $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 //Send the email!
-mail($to,$email_subject,$email_body,$headers);
+mail($to,$email_subject,$email_body);
 //done. redirect to thank-you page.
-header('Location: thanks.html');
 
+header('Location: http://blackrockhistory.net/thanks.html');
 
 // Function to validate against any email injection attempts
 function IsInjected($str)
--- HTML/js/options.js
+++ HTML/js/options.js
@@ -1,4 +1,18 @@
 $('select[name=reason]').change(function () {
+    if ($(this).val() == 'funds') {
+        $('#money').show();
+    } else {
+        $('#money').hide();
+    }
+});
+$('select[name=reason]').change(function () {
+    if ($(this).val() == 'volunteer') {
+        $('#service').show();
+    } else {
+        $('#service').hide();
+    }
+});
+$('select[name=reason]').change(function () {
     if ($(this).val() == 'textSuggest') {
         $('#text').show();
     } else {
